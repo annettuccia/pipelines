d0c06e4c49ee1dc18d5c1d1020f02a5c0006022a
Anna Tsed <tsed.anna@gmail.com>
2026-04-06 18:58:31 +0300

    feat: add data file for api

---
+++ data/bookingData.js
@@ -0,0 +1,23 @@
+export const testBookingData = {
+    "firstname": "Jim",
+    "lastname": "Brown",
+    "totalprice": 666,
+    "depositpaid": true,
+    "bookingdates": {
+        "checkin": "2026-04-01",
+        "checkout": "2026-04-01"
+    },
+    "additionalneeds": "Breakfast and Lunch"
+};
+
+export const updateBookingData = {
+    "firstname": "James",
+    "lastname": "Brown",
+    "totalprice": 777,
+    "depositpaid": true,
+    "bookingdates": {
+        "checkin": "2026-04-01",
+        "checkout": "2026-04-03"
+    },
+    "additionalneeds": "Lunch"
+};
